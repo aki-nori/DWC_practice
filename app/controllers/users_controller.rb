f6c44e18ecee6c7c2f751fe151957b310123439b
aki-nori <i.akinori09@gmail.com>
2020-03-26 12:01:55 +0000

    user_registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = "Welcome to the Sample App!"
+      # リダイレクト直後のページで表示
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
